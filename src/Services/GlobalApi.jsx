3bd5fdfd2cc2501dcb83cc9d5aa23c530634bcac
Hamguy82 <Elvin.x2005@gmail.com>
2024-08-17 18:38:09 +0100

    first commit

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const movieBaseUrl="https://api.themoviedb.org/3"
 
-const api_key='a76ee67773a785b3dfdaeb8677c22ce0'
-const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=a76ee67773a785b3dfdaeb8677c22ce0'
+const api_key='...'
+const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=...'
 //https://api.themoviedb.org/3/movie/550?api_key=a76ee67773a785b3dfdaeb8677c22ce0
 const getTrendingVideos=axios.get(movieBaseUrl + "/trending/all/day?api_key=" + api_key)
 
